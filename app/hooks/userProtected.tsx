ee7c4c7d04a120356aa959326e412a87a73ad60e
Nirjus Karmakar <128937557+Nirjus@users.noreply.github.com>
2024-01-10 01:38:42 +0530

    protected hook update

--- app/hooks/userProtected.tsx
+++ app/hooks/userProtected.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { useSelector } from "react-redux";
 import { redirect } from "next/navigation";
+import UserAuth from "./userAuth";
 
 interface ProtectedRoutes{
     children: React.ReactNode
 }
 export default function Protected({children}: ProtectedRoutes){
-         const {user} = useSelector((state: any) => state.auth);
+        const isLogin = UserAuth();
 
-         return user ? children : redirect("/");
+         return isLogin ? children : redirect("/");
 }
